90c57ebe06840a9e880ddfb726fc2c653f77420f
florian.thibord@gmail.com <mickael.menu@gmail.com>
2012-11-29 17:47:59 +0100

    Comment done on TypeRepr.ml

--- src/TypeRepr.ml
+++ src/TypeRepr.ml
@@ -9,20 +9,23 @@
 open Utils;;
 open Types;;
 
+(** implementation of Types.tuple_type *)
 class tuple_type_repr (ts: valueType list) = object
-  method arity = List.length ts
+  method arity = List.length ts 
   method elements = ts
   method toString = string_of_collection "" "" "*" string_of_valueType ts
 end
 
+(** instantiation of a tuple_type_repr *)
 let makeTupleTypeRepr ts = new tuple_type_repr ts
 
+(** instantiation of a TTuple(...):Types.valueType *)
 let makeTupleType : valueType list -> valueType = 
-  function ts -> TTuple (makeTupleTypeRepr ts)
+  function ts -> TTuple (makeTupleTypeRepr ts) 
   
-
+(** implementation of Types.prim_type *)
 class prim_type_repr (mname:string) (pname:string) (ps : valueType list) (rt : valueType) = object
-  method moduleName = mname
+  method moduleName = mname 
   method primName = pname
   method arity = List.length ps
   method params = ps
@@ -30,6 +33,8 @@ class prim_type_repr (mname:string) (pname:string) (ps : valueType list) (rt : v
   method toString = (string_of_collection "" "" "*" string_of_valueType ps) ^ "->" ^ (string_of_valueType rt)
 end
   
+(** instatiation of a prim_type_repr *)
 let makePrimTypeRepr mname pname ps rt = new prim_type_repr mname pname ps rt
 
+(** instantiation of a TPrim(...):Types.valueType *)
 let makePrimType mname pname ps rt = TPrim (makePrimTypeRepr mname pname ps rt)
