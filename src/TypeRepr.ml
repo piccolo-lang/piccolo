748269db2253f190f1f27d241bfbf80c48b6fd2d
florian.thibord@gmail.com <mickael.menu@gmail.com>
2012-11-30 12:11:07 +0100

    typeRepr v2

--- src/TypeRepr.ml
+++ src/TypeRepr.ml
@@ -6,24 +6,26 @@
 
 *)
 
-open Utils;;
+(** This module defines the implementation of the interfaces defined in Types *)
+
+open Utils;; 
 open Types;;
 
-(** implementation of Types.tuple_type *)
+(** Implementation of tuple_type with a valueType*)
 class tuple_type_repr (ts: valueType list) = object
   method arity = List.length ts 
   method elements = ts
   method toString = string_of_collection "" "" "*" string_of_valueType ts
 end
 
-(** instantiation of a tuple_type_repr *)
+(** Instantiation of a tuple_type_repr *)
 let makeTupleTypeRepr ts = new tuple_type_repr ts
 
-(** instantiation of a TTuple(...):Types.valueType *)
+(** Return a TTuple of type valueType *)
 let makeTupleType : valueType list -> valueType = 
   function ts -> TTuple (makeTupleTypeRepr ts) 
   
-(** implementation of Types.prim_type *)
+(** Implementation of prim_type *)
 class prim_type_repr (mname:string) (pname:string) (ps : valueType list) (rt : valueType) = object
   method moduleName = mname 
   method primName = pname
@@ -33,8 +35,8 @@ class prim_type_repr (mname:string) (pname:string) (ps : valueType list) (rt : v
   method toString = (string_of_collection "" "" "*" string_of_valueType ps) ^ "->" ^ (string_of_valueType rt)
 end
   
-(** instatiation of a prim_type_repr *)
+(** Instantiation of a prim_type_repr with  *)
 let makePrimTypeRepr mname pname ps rt = new prim_type_repr mname pname ps rt
 
-(** instantiation of a TPrim(...):Types.valueType *)
+(** Return a TPrim of type valueType *)
 let makePrimType mname pname ps rt = TPrim (makePrimTypeRepr mname pname ps rt)
