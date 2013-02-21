1d510173eb4858f55676c637d48258a46350a2db
Loïc <loic.girault@gmail.com>
2013-02-21 13:34:47 +0100

    a little frontend

---
+++ src/front.ml
@@ -0,0 +1,9 @@
+
+module CBackend = Backend.Make (SeqASTConstC) (SeqASTPrettyPrinterC)
+
+let _ =
+  let mod_def = ParseUtils.parseFromFile Settings.filename in
+  let (_,errors) = Middleend.first_pass mod_def Settings.verbose in
+  let c_code = CBackend.compile_module mod_def in
+  let formatter = Format.formatter_of_out_channel (open_out Settings.outname) in
+    Format.fprintf formatter "%a@\n" CBackend.print_instr c_code
