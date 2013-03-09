372e27ce342e4ec527add6b73f069439d3918ec2
patrick <patrick@patrick-Inspiron-1012.(none)>
2013-03-09 17:12:47 +0100

    Update for Primitives

--- src/Prim.ml
+++ src/Prim.ml
@@ -1,16 +1,23 @@
 
 open Types
+open TypeRepr
 open SeqAST
 
 type k = string * string
-type v = string * ((valueType list) * valueType)
+type v = string * prim_type_repr
+
 
 let proc_table : (k, v) Hashtbl.t = Hashtbl.create 2
 let value_table : (k, v) Hashtbl.t = Hashtbl.create 2
 
 let _ =
-  Hashtbl.add value_table ("core/arith", "add") ("PICC_Int_add", ([TInt; TInt], TInt))
+Hashtbl.add value_table ("core/arith", "add") ("PICC_Int_add", (makePrimTypeRepr "core/arith/int" "add" [TInt; TInt] TInt));
+Hashtbl.add value_table ("core/arith", "substract") ("PICC_Int_substract", (makePrimTypeRepr "core/arith/int" "substract" [TInt; TInt] TInt));
+Hashtbl.add value_table ("core/arith", "compare") ("compare_values", (makePrimTypeRepr "core/arith/int" "compare" [TInt; TInt] TInt));
+Hashtbl.add value_table ("core/io", "print") ("PICC_print_value_infos", (makePrimTypeRepr "core/io" "print" [TString] TString));;
 
 let get_value_name module_name def_name =
-  let (name, _) = Hashtbl.find value_table (module_name,def_name) in
-  name
+  try
+    let (name, _) = Hashtbl.find value_table (module_name, def_name) in
+      name
+  with _ -> failwith "primitive name unknown"
