ce043a23b69f8028e29f463f84a32c9cbba72148
pokpatrick <2700588@ari-31-305-11.infop6.jussieu.fr>
2013-03-14 19:16:42 +0100

    Refactoring Primitives

---
+++ src/Prims.ml
@@ -0,0 +1,12 @@
+
+let add_name = "PICC_Int_add";;
+
+let substract_name = "PICC_Int_substract";;
+
+let compare_name = "compare_value";;
+
+let print_info_name = "PICC_print_value_infos";;
+
+let print_str_name = "PICC_print_value";;
+
+let print_int_name = "PICC_print_value";;
