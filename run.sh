da796b9b2cd18c9242d2ec896c01f23a060b0b2a
stiganu <postaserg@gmail.com>
2013-03-16 14:09:12 +0100

    added build instructions to compile the generated code in run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/bin/sh 
 
+PIRT_PATH=../../libpirt/
+CFLAGS="-g -Wall -std=c99 -L$PIRT_PATH/lib -I$PIRT_PATH/include"
+LFLAGS="-lpirt -lpthread"
+
 if test $# -eq 1
 then
     cd src
     make
     ./pcc -o ../tests/$1.c -v ../tests/$1.pth
+    gcc $CFLAGS ../tests/$1.c -o $1 $LFLAGS
 else
     echo "Usage:\n ./run.sh mytest\n\t with mystest.pth present in tests/"
 fi
