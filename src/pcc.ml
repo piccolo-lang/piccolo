1f24dd27140ab3391858d025c90d6635177e8cdf
stiganu <postaserg@gmail.com>
2013-03-20 18:44:27 +0100

    more complete correction on the choice of the main definition

--- src/pcc.ml
+++ src/pcc.ml
@@ -8,7 +8,7 @@ let _ =
   let main_def, c_code = CBackend.compile_module mod_def in
   let formatter = Format.formatter_of_out_channel (open_out Settings.outname) in
   Printf.printf "%s\n%!" (m#toString);
-  let entry_point = m#name ^ "_" ^ "Main"(*main_def#name*) in
+  let entry_point = m#name ^ "_" ^ main_def#name in
   Format.fprintf formatter "%a@\n" 
     CBackend.(print_main Settings.nb_thread entry_point Settings.std_gc_fuel Settings.quick_gc_fuel
 		Settings.active_factor main_def#esize main_def#nbChannels main_def#nbChoiceMax) c_code
