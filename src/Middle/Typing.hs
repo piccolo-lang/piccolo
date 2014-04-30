8e485ca459554b412e2dbcc02461bad73a9b0f34
Aurélien Deharbe <aurelien.deharbe@gmail.com>
2014-04-30 16:50:51 +0200

    typing pass is now ready to be written !

--- src/Middle/Typing.hs
+++ src/Middle/Typing.hs
@@ -1,4 +1,4 @@
-module Middle.Typing where
+module Middle.Typing (typingPass) where
 
 import Front.AST
 import Front.ASTUtils
@@ -8,8 +8,24 @@ import Control.Monad.Error
 import Control.Monad.State
 import qualified Data.Map as Map
 
+type TypingDefsEnv = [Definition]
+type TypingVarsEnv = Map.Map String TypeExpr
+type TypingEnv = (TypingDefsEnv, TypingVarsEnv)
+
+type TypingM a = ErrorT PiccError (State TypingEnv) a
+
 typingPass :: ModuleDef -> Either PiccError ModuleDef
-typingPass = undefined
+typingPass mDef = evalState (runErrorT tChecked) initEnv
+  where initEnv  = (moduleDefs mDef, Map.empty)
+        tChecked = tcModule mDef
+
+tcModule :: ModuleDef -> TypingM ModuleDef
+tcModule mDef = do
+  defs <- mapM tcDefinition $ moduleDefs mDef
+  return $ mDef { moduleDefs = defs }
+
+tcDefinition :: Definition -> TypingM Definition
+tcDefinition = error "TODO Middle.Typing.tcDefinition"
 
 {--
 data TypingError = TypingError { tErrExpr :: String
