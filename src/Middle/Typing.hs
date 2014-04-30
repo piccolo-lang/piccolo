dd4964e70623ac5402ae82721c4a8ee0da42d5b4
Aurélien Deharbe <aurelien.deharbe@gmail.com>
2014-04-30 16:54:26 +0200

    typing pass is now ready to be written !

--- src/Middle/Typing.hs
+++ src/Middle/Typing.hs
@@ -27,36 +27,3 @@ tcModule mDef = do
 tcDefinition :: Definition -> TypingM Definition
 tcDefinition = error "TODO Middle.Typing.tcDefinition"
 
-{--
-data TypingError = TypingError { tErrExpr :: String
-                               , tErrLoc  :: Location
-                               , tErrExpected :: TypeExpr
-                               , tErrFound    :: TypeExpr
-                               }
-
-instance Error TypingError where
-  noMsg  = undefined
-  strMsg = undefined
-
-type TypingDefsEnv = [Definition]
-type TypingVarsEnv = Map.Map String TypeExpr
-
-type TypingEnv = (TypingDefsEnv, TypingVarsEnv)
-
-type TypingM a = ErrorT TypingError (State TypingEnv) a
-
-typingPass :: ModuleDef -> ModuleDef
-typingPass mDef = case evalState (runErrorT tChecked) initEnv of
-  Left tErr  -> error "typing error!"
-  Right tAst -> tAst
-  where tChecked = tcModule mDef
-        initEnv  = (moduleDefs mDef, Map.empty)
-
-tcModule :: ModuleDef -> TypingM ModuleDef
-tcModule mDef = do
-  defs <- mapM tcDefinition $ moduleDefs mDef
-  return $ mDef { moduleDefs = defs }
-
-tcDefinition :: Definition -> TypingM Definition
-tcDefinition = error "TODO Middle.Typing.tcDefinition"
--}
